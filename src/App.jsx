ec0fd36388d8449be088b28374a028d01ac66114
Łukasz Nolka <nolkalukasz@gmail.com>
2019-06-06 09:34:57 +0200

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sass/App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+    	<h1>Hello World !</h1>
+    </div>
+  );
+}
+
+export default App;
+
