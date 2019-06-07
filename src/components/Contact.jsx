80d5728174330cf181f9c9d0007ddc47bd64ee44
Łukasz Nolka <nolkalukasz@gmail.com>
2019-06-07 10:40:52 +0200

    Add keys and update syntax

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import './Contact.scss';
 import '../vendor/css/fontello.css';
 
-function Contact(props) {
-    console.log(props.contact);
-    return (
-        <li key={props.contact.id} className="contact">
+const Contact = ({id, firstName, lastName, email}) => (
+    
+        <li key={id} className="contact">
             <div className="avatar icon-user"></div>
             <div className="contact-info">
-                <p className="name">{props.contact.firstName}  {props.contact.lastName}</p>
-                <p className="email">{props.contact.email}</p>
+                <p className="name">{firstName}  {lastName}</p>
+                <p className="email">{email}</p>
             </div>
             <div className="info-ico icon-info"></div>
         </li>
-    );
-  }
+)  
+  
   
   export default Contact;
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -4,41 +4,41 @@ import '../vendor/css/fontello.css';
 
 import Contact from './Contact.jsx';
 
-const contacts = [
-    {
-      id: 1,
-      firstName: 'Jan',
-      lastName: 'Nowak',
-      email: 'jan.nowak@example.com',
-    },
-    {
-      id: 2,
-      firstName: 'Adam',
-      lastName: 'Kowalski',
-      email: 'adam.kowalski@example.com',
-    },
-    {
-      id: 3,
-      firstName: 'Zbigniew',
-      lastName: 'Koziol',
-      email: 'zbigniew.koziol@example.com',
+  class Contacts extends React.Component {
+    state = {
+      contacts : [
+        {
+          id: 1,
+          firstName: 'Jan',
+          lastName: 'Nowak',
+          email: 'jan.nowak@example.com',
+        },
+        {
+          id: 2,
+          firstName: 'Adam',
+          lastName: 'Kowalski',
+          email: 'adam.kowalski@example.com',
+        },
+        {
+          id: 3,
+          firstName: 'Zbigniew',
+          lastName: 'Koziol',
+          email: 'zbigniew.koziol@example.com',
+        }
+      ]
     }
-  ];
 
-  let list = contacts.map(function(contact) {
+    render() {
+      const {contacts} = this.state; //destrukturyzacaja contacts z state (żeby nie używać ciągle this.state)
+
       return (
-        <Contact  contact= {contact} />
-      );
-  });
-
-function Contacts() {
-    return (
         <div className="contacts">
             <ul className="contacts-list">
-               {list}
+            {contacts.map(contact => { return <Contact key={contact.id}  {...contact} />})}
             </ul>
         </div>
     );
-  }
+    }
+  } 
   
   export default Contacts;
